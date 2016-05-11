64893896182ed54a92eb9feb0eef922f71b741ab
rahulrama <rahulrama@gmail.com>
2016-05-11 08:11:18 +0100

    Story 4 completed

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -27,4 +27,10 @@ describe('Feature Test:', function(){
     expect(airport.planes()).toContain(plane)
   })
 
+  it('blocks landing when weather is stormy', function(){
+    spyOn(airport, 'isStormy').and.returnValue(true)
+    expect(function(){plane.land(airport)}).toThrowError('cannot land during storm')
+    expect(airport.planes()).not.toContain(plane)
+  })
+
 })
